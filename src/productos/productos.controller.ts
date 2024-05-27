06ca1292894bd2cf32043196484e657543a248f7
Teofilo Copa <teox777@gmail.com>
2024-05-27 17:44:38 -0400

    Version mejorada

--- src/productos/productos.controller.ts
+++ src/productos/productos.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { ProductosService } from './productos.service';
+import { CrearProductoDto } from './dto/CrearProductoDto';
+import {  ActualizarProductoDto } from './dto/ActualizarProductoDto';
 
 @Controller('productos')
 export class ProductosController {
@@ -22,5 +24,23 @@ export class ProductosController {
         return this.productosService.findById(+id);
     }
 
+    @Post()
+    crearProducto(@Body() crearDto:CrearProductoDto){
+        return this.productosService.create(crearDto);
+
+    }
+    @Patch(":id")
+    actualizarProducto(
+        @Param("id") id:String,
+        @Body() updateDto:ActualizarProductoDto){
+        return this.productosService.update(+id,updateDto);
+    }
+
+    @Delete(":id")
+    eliminarProducto(@Param("id") id:String){
+        return this.productosService.delete(+id);
+    }
 
 }
+
+
